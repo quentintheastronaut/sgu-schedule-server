5f4c23a11026f4babd315935007477ebfb30f433
Nhat Quan <quan260402@gmail.com>
2022-02-18 11:20:29 +0700

    update server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8000;
 var cors = require("cors");
 const request = require("request-promise");
 const cheerio = require("cheerio");
